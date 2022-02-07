2aaaf59d45502ace51d28a193e743d8b1013c2ae
Federico Mazzei <federicomazzei@gmail.com>
2022-02-07 14:32:22 -0300

    guardo pdfs historicos en pdfEjecutados

--- crea_pdf.py
+++ crea_pdf.py
@@ -38,7 +38,7 @@ def armarPDF():
   pdf.set_creator('Federico mazzei')
   pdf.set_author('Federico mazzei')
   pdf.output('etiquetas.pdf', 'F')
-  pdf.output('etiquetas-'+str(now.hour)+'hs'+str(now.minute)+'min' +
+  pdf.output('./pdfEjecutados/etiquetas-'+str(now.hour)+'hs'+str(now.minute)+'min' +
              str(now.second)+'seg'+'-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.pdf', 'F')
 
 armarPDF()
