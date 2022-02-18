89754879f666ab7ef1f8792b79d5708ef63871c6
Federico Mazzei <federicomazzei@gmail.com>
2022-02-18 11:51:11 -0300

    agrego comentarios

--- crea_pdfArgenchemical.py
+++ crea_pdfArgenchemical.py
@@ -8,24 +8,28 @@
 from fpdf import FPDF
 from datetime import datetime
 
+# Para mas información sobre FPDF ir a https://pyfpdf.readthedocs.io/en/latest/Tutorial/index.html
 class PDF(FPDF):
   pass # hace que no pase nada cuando se ejecuta
 
+  # Defino la imagen de fondo para la etiqueta
   def imagen_fondo(self, name, x, y, w, h):
     self.image(name, x, y, w, h)
 
+  # Función para setear el texto del producto a mostrar en la etiqueta
   def producto(self, txt):
     self.set_xy(10.0,30.0)
     self.set_text_color(0, 0, 0)
     self.set_font('Arial', 'B', 22)
     self.multi_cell(0, 10, txt, 0, "C")
 
+  # Función para setear el texto de la cantidad a mostrar en la etiqueta
   def cantidad(self, txt):
     self.set_text_color(0, 0, 0)
     self.set_font('Arial', '',22)
     self.text(16, 73, txt)
 
-
+# Defino la función que arma la etiqueta pasando lo variable necesarias para armarla
 def armarPDF():
   now = datetime.now()
   pdf = PDF('P','mm',(100,80)) #objeto PDF
@@ -48,30 +52,8 @@ def armarPDF():
   pdf.set_creator('Federico mazzei')
   pdf.set_author('Federico mazzei')
   pdf.output('etiquetas.pdf', 'F')
+  # agrego un segundo archivo con la fecha y hora de cuando se ejecutó el script dentro del directorio pdfEjecutados
   pdf.output('./pdfEjecutados/etiquetas-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'-'+str(now.hour)+'hs'+str(now.minute)+'min' +
              str(now.second)+'seg'+'.pdf', 'F')
 
 armarPDF()
-
-
-# def armarPDF():
-#   now = datetime.now()
-#   pdf = PDF('P', 'mm', (100, 80))  # objeto PDF
-#   ruta_archivo_productos = './data.json'
-#   fichero = open(ruta_archivo_productos, encoding="utf-8")
-#   lineas = fichero.readlines()
-#   for linea in lineas:
-#     if linea != "\n":
-#       pdf.add_page()
-#       pdf.imagen_fondo('fondoArgenchemical.png', 0, 0, 100, 80)
-#       pdf.texts(linea.strip())
-#   fichero.close()
-#   pdf.set_title('Etiquetas para productos')
-#   pdf.set_subject(
-#       'Este documento contiene las etiquetas generadas por Federico Mazzei')
-#   pdf.set_keywords('etiquetas')
-#   pdf.set_creator('Federico mazzei')
-#   pdf.set_author('Federico mazzei')
-#   pdf.output('etiquetas.pdf', 'F')
-#   pdf.output('./pdfEjecutados/etiquetas-'+str(now.hour)+'hs'+str(now.minute)+'min' +
-#              str(now.second)+'seg'+'-'+str(now.day)+'-'+str(now.month)+'-'+str(now.year)+'.pdf', 'F')
